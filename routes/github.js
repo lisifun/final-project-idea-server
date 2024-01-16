7c96663ad4d0ae12048dfa98d934c9ec021b3ade
lisifun <lisandrafj99@gmail.com>
2024-01-16 18:07:20 -0500

    testing

--- routes/github.js
+++ routes/github.js
@@ -4,6 +4,7 @@ const router = express.Router();
 /* GET github listing. */
 router.post("/", function (req, res, next) {
   console.log(req.body);
+  console.log("hiii");
   res.send("respond with a resource");
 });
 
